4372b14da02e0bc367cc17c2ee72a19852e03f32
Javier Cabanillas <jcabanillas@rubiko.mx>
2021-06-14 11:00:33 -0500

    Minor fix

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -115,7 +115,9 @@
         var count = _count($elem, widgetOptions);
 
         if (count < widgetOptions.limit) {
-            $toclone = $(widgetOptions.template);
+            // $toclone = $(widgetOptions.template);
+            // $toclone = $(widgetOptions.template);
+            $toclone = _parseTemplate(widgetOptions);
             $newclone = $toclone.clone(false, false);
 
             if (widgetOptions.insertPosition === 'top') {
